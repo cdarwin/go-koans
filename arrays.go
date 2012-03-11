ca53462aae801199c15d869ca8d6cd50860fb1c2
Steven Degutis <steven.degutis@gmail.com>
2012-03-11 00:08:47 -0600

    pretty sweet

--- arrays.go
+++ arrays.go
@@ -13,4 +13,15 @@ func testArrays() {
 
   assert(len(fruits) == __int__) // the length is what the length is
   assert(cap(fruits) == __int__) // it can hold no more
+
+  tasty_fruits := fruits[1:3] // defining ones self as a variation of another
+  assert(tasty_fruits[0] == __string__) // slices of arrays share some identity
+  assert(tasty_fruits[1] == __string__) // albeit slightly askewed
+
+  tasty_fruits[0] = "lemon" // but are their shared roots truly identical?
+
+  assert(fruits[0] == __string__) // has this remained the same?
+  assert(fruits[1] == __string__) // how about this?
+  assert(fruits[2] == __string__) // surely one of these must have changed
+  assert(fruits[3] == __string__) // but who can know these things
 }
