7171007732a207266206269bfe6d4ae5e292c352
Steven Degutis <steven.degutis@gmail.com>
2012-03-29 07:20:11 -0500

    make it more clear we're moving into panic mode

--- about_panics.go
+++ about_panics.go
@@ -4,7 +4,11 @@ func divideFourBy(i int) int {
   return 4 / i
 }
 
+const __divisor__ = 0
+
 func aboutPanics() {
-  n := divideFourBy(0)
+  assert(__delete_me__) // panics are exceptional errors at runtime
+
+  n := divideFourBy(__divisor__)
   assert(n == 2) // panics are exceptional errors at runtime
 }
