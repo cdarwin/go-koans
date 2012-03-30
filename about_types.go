1d1a88d773678f21ab5dbf05c225f3bbe580b47f
Steven Degutis <steven.degutis@gmail.com>
2012-03-30 15:25:39 -0500

    changing method name for less confusion

--- about_types.go
+++ about_types.go
@@ -2,12 +2,12 @@ package go_koans
 
 type coolNumber int
 
-func (cn coolNumber) double() int {
+func (cn coolNumber) multiplyByTwo() int {
   return int(cn) * 2
 }
 
 func aboutTypes() {
   i := coolNumber(4)
   assert(i == coolNumber(__int__)) // values can be converted between compatible types
-  assert(i.double() == __int__)    // you can add methods on any type you define
+  assert(i.multiplyByTwo() == __int__)    // you can add methods on any type you define
 }
