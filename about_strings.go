6e90c6c1406d6a4ef5337b9dccb5670f2703ebf2
Ray Sohn <raymondsohn@gmail.com>
2013-09-08 18:06:48 -0400

    about_string.go: fix contraction, about_control_flow.go: placing "abc"[0] in assert causes internal compiler error, assigned it to variable

--- about_strings.go
+++ about_strings.go
@@ -6,7 +6,8 @@ func aboutStrings() {
   assert("a"+__string__ == "abc") // string concatenation need not be difficult
   assert(len("abc") == __int__)   // and bounds are thoroughly checked
 
-  assert("abc"[0] == __byte__) // their contents are reminiscent of C
+  c := "abc"[0]
+  assert(c == __byte__) // their contents are reminiscent of C
 
   assert("smith"[2:] == __string__)  // slicing may omit the end point
   assert("smith"[:4] == __string__)  // or the beginning
